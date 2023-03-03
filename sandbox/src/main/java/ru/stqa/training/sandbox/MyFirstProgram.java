8e8ac9d3264b156b1c2f55d8b0d7c4ba0bb385c4
Sash <softester@yandex.ru>
2023-03-03 08:06:36 +0300

    Работа с типами переменных

--- sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
@@ -3,10 +3,11 @@ package ru.stqa.training.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
 
-    int i = 8;
-    int s = i * i;
+    double i = 8.0;
+    double s = i * i;
     System.out.println("Площадь квадрата со стороной " + i + " равна " + s);
   }
 
