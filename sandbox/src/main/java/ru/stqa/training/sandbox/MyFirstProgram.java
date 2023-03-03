d0521b2dab6dc0c6026a9784835c79491ba8401a
Sash <softester@yandex.ru>
2023-03-03 07:59:19 +0300

    Добавлены переменные при выводе на экран

--- sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
@@ -4,6 +4,10 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+
+    int i = 8;
+    int s = i * i;
+    System.out.println("Площадь квадрата со стороной " + i + " равна " + s);
   }
 
 }
