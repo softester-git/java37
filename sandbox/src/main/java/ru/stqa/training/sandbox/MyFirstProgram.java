5b9bda230cddb1fbf4c07a95322e4878a94c7dfa
Sash <softester@yandex.ru>
2023-03-05 20:06:08 +0300

    Вычисление расстояния реализовано с использованием метода в классе Point

--- sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
@@ -13,21 +13,12 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоуголгника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 3;
-    p1.y = 5;
-    p2.x = 6;
-    p2.y = 8;
-    System.out.println("Расстояние между точками с координатами " + p1.x + ", " + p1.y + " и " + p2.x + ", " + p2.y + " составляет: " + distance(p1, p2));
+    Point p = new Point(3, 5, 6, 8);
+    System.out.println("Расстояние между точками с координатами " + p.x1 + ", " + p.y1 + " и " + p.x2 + ", " + p.y2 + " составляет: " + p.distance());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) + (p2.y - p1.y));
   }
 
 }
