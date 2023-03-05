47ce0f2019c59e88a767098a3e61e8a65311266d
Sash <softester@yandex.ru>
2023-03-05 18:23:49 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
@@ -4,10 +4,26 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     hello("world");
+    hello("user");
+    hello("Sash");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоуголгника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
