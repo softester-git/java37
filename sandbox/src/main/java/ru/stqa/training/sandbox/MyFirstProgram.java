d818172fc27a261ca9af7ecf5e25941f6043ab8e
Sash <softester@yandex.ru>
2023-03-06 07:46:19 +0300

    Ошибка в классе и методе исправлена

--- sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/MyFirstProgram.java
@@ -13,8 +13,9 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоуголгника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p = new Point(3, 5, 6, 8);
-    System.out.println("Расстояние между точками с координатами " + p.x1 + ", " + p.y1 + " и " + p.x2 + ", " + p.y2 + " составляет: " + p.distance());
+    Point p1 = new Point(3, 5);
+    Point p2 = new Point(6, 8);
+    System.out.println("Расстояние между точками с координатами " + p1.x + ", " + p1.y + " и " + p2.x + ", " + p2.y + " составляет: " + Point.distance(p1, p2));
   }
 
   public static void hello(String somebody) {
