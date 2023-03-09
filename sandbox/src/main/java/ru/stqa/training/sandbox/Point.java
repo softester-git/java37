662cbc57b91a94185a6fed5100a0cc87a6b27f9f
Sash <softester@yandex.ru>
2023-03-09 06:11:17 +0300

    Исправлена формула расчёта расстояния

--- sandbox/src/main/java/ru/stqa/training/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/Point.java
@@ -11,7 +11,7 @@ public class Point {
   }
 
   public double distance(Point p) {
-    return Math.sqrt((p.x - this.x) + (p.y - this.y));
+    return Math.sqrt(Math.pow((p.x - this.x), 2) + Math.pow((p.y - this.y), 2));
   }
 
 }
