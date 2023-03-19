f4e8ee4ec4a63d35067b257cd366a63d1434259f
Sash <softester@yandex.ru>
2023-03-19 20:00:35 +0300

    Реализована проверка и обеспечение выполнения предусловий для групп

--- addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.training.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.training.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Group1", "Group1 header", "Group1 footer"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Group1", "Group1 header", "Group1 footer"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Edited Name",
