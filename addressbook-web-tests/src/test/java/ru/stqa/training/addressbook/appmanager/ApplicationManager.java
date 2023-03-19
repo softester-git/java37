58e7cac43498c33b12601dffe20abeff304fecd1
Sash <softester@yandex.ru>
2023-03-19 18:14:11 +0300

    Знак равенства заменён на equals при сравнении строк

--- addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/appmanager/ApplicationManager.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -26,10 +27,10 @@ public class ApplicationManager {
   public void init() {
     if (browser == BrowserType.FIREFOX) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.CHROME) {
+    } else if (Objects.equals(browser, BrowserType.CHROME)) {
       System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.IE) {
+    } else if (Objects.equals(browser, BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
 
