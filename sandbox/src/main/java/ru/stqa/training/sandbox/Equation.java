fd512832b67175875d48e445df251d93d6342e33
Sash <softester@yandex.ru>
2023-03-19 13:31:07 +0300

    Улучшенная форма if else

--- sandbox/src/main/java/ru/stqa/training/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/Equation.java
@@ -15,12 +15,25 @@ public class Equation {
 
     double d = b*b - 4*a*c;
 
-    if (d > 0) {
-      n = 2;
-    } else if (d == 0) {
-      n = 1;
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        }
+        else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
     } else {
-      n = 0;
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
+        n = 1;
+      } else {
+        n = 0;
+      }
     }
 
   }
