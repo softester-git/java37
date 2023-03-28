b6aa996105d2a928c5e10cd86f7dcbfc4eb29486
Sash <softester@yandex.ru>
2023-03-28 05:10:55 +0300

    Добавлена параметризация тестовых данных для групп

--- addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupCreationTests.java
@@ -4,18 +4,26 @@ import org.testng.annotations.*;
 import ru.stqa.training.addressbook.model.GroupData;
 import ru.stqa.training.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
-    GroupData group = new GroupData()
-            .withName("GroupName")
-            .withHeader("GroupHeader")
-            .withFooter("GroupFooter");
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("name1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("name2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("name3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
+  }
 
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
