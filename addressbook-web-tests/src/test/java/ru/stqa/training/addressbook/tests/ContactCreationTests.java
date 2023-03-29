489351647b5bfb084b1523142a4e4ee4d13ad70d
Sash <softester@yandex.ru>
2023-03-29 15:33:34 +0300

    Добавлена загрузка тестовых данных из CSV файла для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/training/addressbook/tests/ContactCreationTests.java
@@ -3,8 +3,15 @@ package ru.stqa.training.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.training.addressbook.model.ContactData;
 import ru.stqa.training.addressbook.model.Contacts;
+import ru.stqa.training.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,19 +19,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.csv"));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData()
+              .withFirstName(split[0])
+              .withLastName(split[1])
+              .withAddress(split[2])
+              .withHomePhone(split[3])
+              .withEmail(split[4])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     File photo = new File("src" + File.separator
             + "test" + File.separator
             + "resources" + File.separator
             + "stru.jpeg");
-    ContactData contact = new ContactData()
-            .withFirstName("FirstName")
-            .withLastName("LastName")
-            .withAddress("Address")
-            .withHomePhone("1000")
-            .withEmail("test@email.test")
-            .withPhoto(photo);
+    contact.withPhoto(photo);
 
     app.goTo().contactPage();
     Contacts before = app.contact().all();
