8613f69cd83e2b669c84ea8c44005744b69a3139
Sash <softester@yandex.ru>
2023-05-14 08:07:35 +0300

    REST интеграция с Github

---
+++ github-sample/src/test/java/ru/stqa/training/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.training.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("token");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("account_name", "project_name")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
